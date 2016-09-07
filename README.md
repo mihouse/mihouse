f70cd010c4a1e743924d41dd3446afc4a335d4c8
mihouse <osyaberi_michan@yahoo.co.jp>
2016-09-07 09:21:32 +0900

    mihouse.c

--- README.md
+++ README.md
@@ -4,8 +4,20 @@ mihouse
 mihouse
 
 
+Sample
+------
+
+
+
+Relations
+---------
+
+mihouse https://github.com/mihouse/mihouse
+
+
 License
 -------
 
 MIT License
+
 
---
+++ src/mihouse.c
@@ -0,0 +1,16 @@
+/*
+  mihouse.c
+
+  > mingw32-make -f makefile.mingw32
+*/
+
+#include <stdio.h>
+
+int main(int ac, char **av)
+{
+  int i;
+  printf("sizeof(size_t): %08x\n", sizeof(size_t));
+  for(i = 0; i < ac; ++i)
+    printf("%d %s\n", i, av[i]);
+  return 0;
+}
